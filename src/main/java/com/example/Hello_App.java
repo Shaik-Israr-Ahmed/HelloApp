efa54197896e1efe791e019df079dab09330e209
Shaik Israr <shaikisrarahmed786@gmail.com>
2026-04-10 14:00:15 +0530

    UC5 implementation

--- src/main/java/com/example/Hello_App.java
+++ src/main/java/com/example/Hello_App.java
@@ -3,10 +3,22 @@ package com.example;
 public class Hello_App {
     public static void main(String[] args) {
     	String names;
+
         if (args.length == 0) {
             names = "World";
         } else {
-            names = String.join(", ", args);
+            StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
+
+            for (String name : args) {
+                if (!first) {
+                    nameBuilder.append(", ");
+                }
+                nameBuilder.append(name);
+                first = false;
+            }
+
+            names = nameBuilder.toString();
         }
 
         System.out.println("Hello, " + names + "!");
